156a6f79ee2f8a528a96c4301c2290c86ab32bd9
willy950205 <willy950205@gmail.com>
2021-05-04 18:24:09 +0900

    210504 study at work

---
+++ learn-sequelize/models/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const Comment = require('./comment');
+const User = require('./user');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize);
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
+
+module.exports = db;
